318052cfd05be318b1dfcd1bc64fe35c21ee13be
dhrtjr9 <dhrtjr9@gmail.com>
2020-12-07 10:14:31 +0900

    DataType
    DataType

---
+++ DataTvpe/Datatype.java
@@ -0,0 +1,17 @@
+public class Datatype {
+
+    public static void main(String[] args) {
+
+        byte num1 = 127;
+		byte num2 = -128;
+
+		num1++; // 127 + 1
+		num2--; // -128 - 1
+
+        System.out.println(num1); // -128
+
+        System.out.println(num2); // 127
+
+    }
+
+}
